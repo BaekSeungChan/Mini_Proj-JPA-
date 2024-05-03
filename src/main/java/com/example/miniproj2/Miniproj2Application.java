4f7ca45ae0d31bc1ff7d6f5bcf5ef704fd1b3dab
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-05-03 15:15:20 +0900

    Setting

---
+++ src/main/java/com/example/miniproj2/Miniproj2Application.java
@@ -0,0 +1,13 @@
+package com.example.miniproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miniproj2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniproj2/Miniproj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
